a21c2db213e1d3da9f05206729491436fb4b7281
Matthew Nienow <mnienow@pm.me>
2023-02-24 10:19:56 -0600

    Changed to print from html/css.

--- generate.umd.js
+++ generate.umd.js
@@ -2,30 +2,59 @@ const DAY = 1000 * 60 * 60 * 24;
 var DAYS = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 var MONTHS = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
-function generate(options = {}) {
-    const doc = new window.jspdf.default({format: 'letter'});
-    doc.setFont({fontWeight: 'bold'});
-    doc.setFontSize(32);
-    var pageHeight = doc.internal.pageSize.height || doc.internal.pageSize.getHeight();
-    var pageWidth = doc.internal.pageSize.width || doc.internal.pageSize.getWidth();
+const params = new URLSearchParams(window.location.search);
 
-    const startDate = new Date('2023-01-01T00:00:00');
-    const endDate = new Date('2023-02-01T00:00:00');
-    const startTime = 8;
-    const endTime = 17;
 
-    const numDays = Math.round((endDate.getTime() - startDate.getTime()) / DAY);
-    // doc.save("a4.pdf");
+const startDate = new Date(`${params.get('startDate')}T00:00:00`);
+const endDate = new Date(`${params.get('endDate')}T00:00:00`);
+const startTime = Number(params.get('startTime'));
+const endTime = Number(params.get('endTime'));
+const linesPerHour = Number(params.get('lines') || 2);
 
-    for (let i = 0; i < numDays; i++) {
-        const day = new Date(startDate.getTime() + DAY * i);
-        const title = `${DAYS[day.getDay()]}, ${MONTHS[day.getMonth()]} ${day.getDate()}`;
-        doc.text(title, pageWidth / 2, 25, {align: 'center'});
-        doc.addPage('letter');
+const numDays = Math.round((endDate.getTime() - startDate.getTime()) / DAY);
+const numHours = endTime - startTime;
+const numLines = numHours * linesPerHour;
+const lineHeight = 8.5 / numLines;
+const line = `<div class="line" style="height: ${lineHeight}in"></div>`;
+
+let body = '';
+for (let i = 0; i < numDays + 1; i++) {
+    const day = new Date(startDate.getTime() + DAY * i);
+    const title = `<div class="title">${DAYS[day.getDay()]}, ${MONTHS[day.getMonth()]} ${day.getDate()}</div>`;
+
+    const hours = [];
+    for (let j = startTime; j < endTime; j++) {
+        const hour = j === 12 ? 12 : j % 12;
+        // const hourWith0 = hour < 10 ? '0' + hour : hour;
+        // const ampm = j < 12 ? 'AM' : 'PM';
+        const time = hour;
+        hours.push(`<div class="hour">
+            <div class="time">${time}</div>
+            ${line}
+        </div>`);
+        for (let k = 1; k < linesPerHour; k++) {
+            hours.push(`<div class="hour">
+                <div class="time"></div>
+                ${line}
+            </div>`);
+        }
     }
 
-    const output = doc.output('datauristring');
-    document.getElementById('output').src = output + '#toolbar=0&navpanes=0';
+    const page = `<div class="page">${title}${hours.join('')}</div>`;
+    body += page;
+
 }
 
+document.getElementById('output').innerHTML = body;
+
+
+function print() {
+    var prtContent = document.getElementById('output');
+    var WinPrint = window.open('', '', 'toolbar=0,scrollbars=0,status=0');
+    WinPrint.document.write(prtContent.innerHTML);
+    WinPrint.document.close();
+    WinPrint.focus();
+    WinPrint.print();
+    WinPrint.close();
+}
 
--- index.umd.js
+++ index.umd.js
@@ -1,31 +1,15 @@
-const DAY = 1000 * 60 * 60 * 24;
-var DAYS = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-var MONTHS = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+const form = document.forms.generate;
+const startDate = form.startDate;
+const endDate = form.endDate;
+const today = new Date(new Date().toDateString());
+startDate.valueAsDate = today;
+today.setMonth(today.getMonth() + 3);
+endDate.valueAsDate = today;
 
-function generate(options = {}) {
-    const doc = new window.jspdf.default({format: 'letter'});
-    doc.setFont({fontWeight: 'bold'});
-    doc.setFontSize(32);
-    var pageHeight = doc.internal.pageSize.height || doc.internal.pageSize.getHeight();
-    var pageWidth = doc.internal.pageSize.width || doc.internal.pageSize.getWidth();
-
-    const startDate = new Date('2023-01-01T00:00:00');
-    const endDate = new Date('2023-02-01T00:00:00');
-    const startTime = 8;
-    const endTime = 17;
-
-    const numDays = Math.round((endDate.getTime() - startDate.getTime()) / DAY);
-    // doc.save("a4.pdf");
-
-    for (let i = 0; i < numDays; i++) {
-        const day = new Date(startDate.getTime() + DAY * i);
-        const title = `${DAYS[day.getDay()]}, ${MONTHS[day.getMonth()]} ${day.getDate()}`;
-        doc.text(title, pageWidth / 2, 25, {align: 'center'});
-        doc.addPage('letter');
-    }
-
-    const output = doc.output('datauristring');
-    document.getElementById('output').src = output + '#toolbar=0&navpanes=0';
-}
+// function generate()  {
+//     alert('gen');
+//     // window.open(`/generate.html?start=${startDate.value}&end=${endDate.value}`, '_blank');
+//     return false;
+// };
 
 
