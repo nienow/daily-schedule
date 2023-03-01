f4327a4b50173ba7c0acdacc8edb200ac8c1466a
nienow <system@randombits.dev>
2023-03-01 07:39:13 -0600

    Clean up bash script

--- .github/commit.sh
+++ .github/commit.sh
@@ -6,18 +6,11 @@ set -u  # script fails if trying to access an undefined variable
 echo
 echo "##### Starting #####"
 SOURCE_FILES="dist/."
-DESTINATION_USERNAME="nienow"
-DESTINATION_REPOSITORY="randombits"
+DESTINATION_REPOSITORY="nienow/randombits"
 DESTINATION_BRANCH="main"
 DESTINATION_DIRECTORY="content/print-schedule"
 COMMIT_USERNAME="system"
 COMMIT_EMAIL="system@randombits.dev"
-COMMIT_MESSAGE="Updating content from jari"
-
-if [ -z "$COMMIT_USERNAME" ]
-then
-  COMMIT_USERNAME="$DESTINATION_USERNAME"
-fi
 
 CLONE_DIRECTORY=$(mktemp -d)
 
@@ -25,20 +18,12 @@ echo
 echo "##### Cloning source git repository #####"
 # Setup git
 git config --global user.email "$COMMIT_EMAIL"
-git config --global user.name "$DESTINATION_USERNAME"
-
-# Remove git directory if it exists to prevent errors
-#rm -rf .git
-
-#git clone "https://github.com/$GITHUB_REPOSITORY.git" repo
-
-#cd repo
-#ls -la
+git config --global user.name "$COMMIT_USERNAME"
 
 echo
 echo "##### Cloning destination git repository #####"
 
-git clone --single-branch --branch "$DESTINATION_BRANCH" "https://$API_TOKEN_GITHUB@github.com/$DESTINATION_USERNAME/$DESTINATION_REPOSITORY.git" "$CLONE_DIRECTORY"
+git clone --single-branch --branch "$DESTINATION_BRANCH" "https://$API_TOKEN_GITHUB@github.com/$DESTINATION_REPOSITORY.git" "$CLONE_DIRECTORY"
 ls -la "$CLONE_DIRECTORY"
 
 echo
@@ -51,14 +36,11 @@ cd "$CLONE_DIRECTORY"
 echo
 echo "##### Adding git commit #####"
 
-ORIGIN_COMMIT="https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
-
 git add .
 git status
 
 # don't commit if no changes were made
-git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
+git diff-index --quiet HEAD || git commit --message "Update from $GITHUB_REPOSITORY - $GITHUB_SHA"
 
 echo
 echo "##### Pushing git commit #####"
