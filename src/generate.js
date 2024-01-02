7ce73146ad3f9b78127dfec2b17b88b73141678b
Matthew Nienow <mnienow@pm.me>
2024-01-01 20:44:01 -0600

    Making it more flexible

--- src/generate.js
+++ src/generate.js
@@ -1,48 +1,51 @@
 const DAY = 1000 * 60 * 60 * 24;
-var DAYS = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-var MONTHS = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+const DAYS = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const MONTHS = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
-const params = new URLSearchParams(window.location.search);
+export const getParams = () => {
+    const form = document.forms.generate;
+    const startDate = new Date(`${form.startDate.value}T00:00:00`);
+    const endDate = new Date(`${form.endDate.value}T00:00:00`);
+    const prefill = [];
+    form.prefill?.forEach(input => {
+        prefill.push(input.value);
+    });
+    return {startDate, endDate, prefill};
+};
 
-const startDate = new Date(`${params.get('startDate')}T00:00:00`);
-const endDate = new Date(`${params.get('endDate')}T00:00:00`);
-const startTime = Number(params.get('startTime'));
-const endTime = Number(params.get('endTime'));
-const linesPerHour = Number(params.get('lines') || 2);
+export const generate = () => {
+    const {startDate, endDate, prefill} = getParams();
 
-const numDays = Math.round((endDate.getTime() - startDate.getTime()) / DAY);
-const numHours = endTime - startTime;
-const numLines = numHours * linesPerHour;
-const lineHeight = 8.5 / numLines;
-const line = `<div class="line" style="height: ${lineHeight}in"></div>`;
+    const numDays = Math.round((endDate.getTime() - startDate.getTime()) / DAY);
+    const numLines = prefill.length;
+    const lineHeight = 8.5 / numLines;
+    // const line = `<div class="line" style="height: ${lineHeight}in"></div>`;
 
-let body = '';
-for (let i = 0; i < numDays + 1; i++) {
-    const day = new Date(startDate.getTime() + DAY * i);
-    const title = `<div class="title">${DAYS[day.getDay()]}, ${MONTHS[day.getMonth()]} ${day.getDate()}</div>`;
+    let body = '';
+    for (let i = 0; i < numDays + 1; i++) {
+        const day = new Date(startDate.getTime() + DAY * i);
+        const title = `<div class="title">${DAYS[day.getDay()]}, ${MONTHS[day.getMonth()]} ${day.getDate()}</div>`;
 
-    const hours = [];
-    for (let j = startTime; j < endTime; j++) {
-        const hour = j === 12 ? 12 : j % 12;
-        // const hourWith0 = hour < 10 ? '0' + hour : hour;
-        // const ampm = j < 12 ? 'AM' : 'PM';
-        const time = hour;
-        hours.push(`<div class="hour">
-            <div class="time">${time}</div>
-            ${line}
-        </div>`);
-        for (let k = 1; k < linesPerHour; k++) {
-            hours.push(`<div class="hour">
-                <div class="time"></div>
-                ${line}
-            </div>`);
-        }
+        const hours = prefill.map(txt => {
+return `<div class="line" style="height: ${lineHeight}in"><div class="txt">${txt}</div></div>`;
+        });
+        // for (let j = startTime; j < endTime; j++) {
+        //     const hour = j === 12 ? 12 : j % 12;
+        //     // const hourWith0 = hour < 10 ? '0' + hour : hour;
+        //     // const ampm = j < 12 ? 'AM' : 'PM';
+        //     const time = hour;
+        //     hours.push(`<div class="line" style="height: ${lineHeight}in"><div class="txt">${hour}</div></div>`);
+        //     for (let k = 1; k < linesPerHour; k++) {
+        //         hours.push(`<div class="line" style="height: ${lineHeight}in"></div>`);
+        //     }
+        // }
+
+        const page = `<div class="page">${title}${hours.join('')}</div>`;
+        body += page;
+
     }
 
-    const page = `<div class="page">${title}${hours.join('')}</div>`;
-    body += page;
+    return body;
+};
 
-}
-
-document.getElementById('output').innerHTML = body;
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,54 @@
+import {generate, getParams} from "./generate.js";
+
 function preview(event)  {
     event.preventDefault();
-    const formData = new FormData(document.forms.generate);
-    const queryParams = new URLSearchParams(formData).toString();
-    document.getElementById('output').src = '/_generate.html?' + queryParams;
-    document.getElementById('printBtn').style.visibility = 'visible';
+    const params = getParams();
+    window.history.pushState({params}, null);
+
+    const body = generate();
+    document.getElementById('output').contentWindow.document.body.innerHTML = body;
+    // const formData = new FormData(document.forms.generate);
+    // const queryParams = new URLSearchParams(formData).toString();
+    // document.getElementById('output').src = '/_generate.html?' + queryParams;
+    // document.getElementById('printBtn').style.visibility = 'visible';
     document.getElementById('output').parentElement.style.visibility = 'visible';
+    document.forms.generate.style.display = 'none';
 }
+
+window.addEventListener('popstate', event => {
+    console.log(event);
+    const params = event.state?.params;
+    if (params) {
+        const body = generate();
+        document.getElementById('output').contentWindow.document.body.innerHTML = body;
+        document.getElementById('output').parentElement.style.visibility = 'visible';
+        document.forms.generate.style.display = 'none';
+
+    } else {
+        document.getElementById('output').contentWindow.document.body.innerHTML = '';
+        document.getElementById('output').parentElement.style.visibility = 'hidden';
+        document.forms.generate.style.display = 'block';
+    }
+})
 
 function print(event) {
     event.preventDefault();
     document.getElementById('output').contentWindow.print();
+}
+
+function writePrefill() {
+    const fragment = document.createDocumentFragment();
+    const template = document.getElementById('prefillTemplate');
+    for (let j = 8; j < 17; j++) {
+        const hour = j === 12 ? 12 : j % 12;
+        const el = template.content.cloneNode(true);
+        el.querySelector('input').value = '' + hour;
+        fragment.appendChild(el);
+        for (let k = 1; k < 2; k++) {
+            fragment.appendChild(template.content.cloneNode(true));
+        }
+    }
+    document.getElementById('prefill').appendChild(fragment);
 }
 
 function init() {
@@ -20,9 +59,23 @@ function init() {
     startDate.valueAsDate = today;
     today.setMonth(today.getMonth() + 3);
     endDate.valueAsDate = today;
+    writePrefill();
 
     document.getElementById('previewBtn').addEventListener('click', preview);
     document.getElementById('printBtn').addEventListener('click', print);
+    document.getElementById('backBtn').addEventListener('click', () => {
+        window.history.back();
+    });
+    document.getElementById('addBtn').addEventListener('click', (e) => {
+        e.preventDefault();
+        document.getElementById('prefill').appendChild(document.getElementById('prefillTemplate').content.cloneNode(true));
+    });
+    document.getElementById('prefill').addEventListener('click', (e) => {
+        e.preventDefault();
+        if (e.target.name === 'removePrefill') {
+            e.target.parentElement.remove();
+        }
+    });
 }
 
 init();
