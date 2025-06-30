342ed05f5f7d50135c44692b0c9260ec863d5879
Matthew Nienow <mnienow@pm.me>
2025-06-30 12:05:25 -0500

    Upgrade to vite v7

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,23 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite';
 
 export default defineConfig({
-    base: '/daily-schedule/',
-    build: {
-        polyfillModulePreload: false,
-
-        rollupOptions: {
-            input: {
-                index: 'index.html',
-                _generate: '_generate.html',
-            },
-            output: {
-                dir: 'dist',
-                assetFileNames: '[name]-[hash][extname]',
-                chunkFileNames: '[name]-[hash][extname]',
-                entryFileNames: '[name]-[hash].js'
-            },
-        },
+  base: '/daily-schedule/',
+  build: {
+    modulePreload: {
+      polyfill: false
     },
-})
+
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        _generate: '_generate.html',
+      },
+      output: {
+        dir: 'dist',
+        assetFileNames: '[name]-[hash][extname]',
+        chunkFileNames: '[name]-[hash][extname]',
+        entryFileNames: '[name]-[hash].js'
+      },
+    },
+  },
+});
